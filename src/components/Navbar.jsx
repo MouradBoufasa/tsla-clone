fb0111cdaed40173fe76c6ae63f869a92250ba1b
Mourad Boufasa <boufasamourad@outlook.fr>
2022-10-23 19:22:35 +0100

    my first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,140 @@
+import { Close } from "@material-ui/icons";
+import { useState } from "react";
+import styled from "styled-components";
+import logo from "../images/logo.svg";
+
+const Navbar = () => {
+  const [menu, setMenu] = useState(false);
+  return (
+    <Nav>
+      <Nav_Logo_Cntr>
+        <Nav_Logo src={logo} />
+      </Nav_Logo_Cntr>
+      <Nav_Menu>
+        <Models>Model S</Models>
+        <Models>Model Y</Models>
+        <Models>Model X</Models>
+        <Models>Model 3</Models>
+        <Models>Solar Roofs</Models>
+        <Models>Solar Panels</Models>
+      </Nav_Menu>
+      <Nav_Menu_Right>
+        <Models>Shop</Models>
+        <Models>Account</Models>
+        <Models onClick={() => setMenu(!menu)}>Menu</Models>
+      </Nav_Menu_Right>
+      <Nav_Full_Menu menu={menu}>
+        <Icon onClick={() => setMenu(!menu)}>
+          <Close />
+        </Icon>
+        <Nav_Link>Existing Inventory</Nav_Link>
+        <Nav_Link>Used Inventory</Nav_Link>
+        <Nav_Link>Trade-In</Nav_Link>
+        <Nav_Link>Test Drive</Nav_Link>
+        <Nav_Link>Cybertruck</Nav_Link>
+        <Nav_Link>Roadster</Nav_Link>
+        <Nav_Link>Semi</Nav_Link>
+        <Nav_Link>Charging</Nav_Link>
+        <Nav_Link>Powerwall</Nav_Link>
+        <Nav_Link>Commercial Energy</Nav_Link>
+        <Nav_Link>Utilities</Nav_Link>
+        <Nav_Link>Find Us</Nav_Link>
+        <Nav_Link>Support</Nav_Link>
+        <Nav_Link>Investor Relations</Nav_Link>
+      </Nav_Full_Menu>
+    </Nav>
+  );
+};
+
+export default Navbar;
+
+const Nav = styled.div`
+  min-height: 60px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  display: flex;
+  align-items: center;
+  padding: 0 40px;
+  width: 100%;
+  justify-content: space-between;
+`;
+const Nav_Logo = styled.img`
+  height: 15px;
+  width: 150px;
+`;
+const Nav_Menu = styled.div`
+  display: flex;
+  align-items: center;
+  flex: 2;
+  width: 100%;
+  justify-content: center;
+`;
+const Nav_Logo_Cntr = styled.div`
+  display: flex;
+  align-items: center;
+  flex: 1;
+  width: 100%;
+  justify-content: flex-start;
+`;
+const Nav_Menu_Right = styled.div`
+  display: flex;
+  align-items: center;
+  flex: 1;
+  justify-content: flex-end;
+`;
+const Models = styled.p`
+  margin: 0 5px;
+  font-weight: 600;
+  cursor: pointer;
+  transition: background 0.2s ease-in-out;
+  padding: 10px 15px;
+  border-radius: 10px;
+  font-size: 14px;
+  :hover {
+    background: #dfdfdf6c;
+    border-radius: 10px;
+  }
+`;
+
+const Nav_Full_Menu = styled.div`
+  position: absolute;
+  top: 0;
+  right: ${(props) => (props.menu ? "0" : "-20%")};
+  height: 100vh;
+  background: #fff;
+  width: 20%;
+  padding: 10px 20px;
+  transition: all 0.8s ease;
+  opacity: ${(props) => (props.menu ? "1" : "0.4")};
+`;
+const Nav_Link = styled.p`
+  padding: 5px 15px;
+  margin-top: 15px;
+  border-radius: 10px;
+  font-size: 16px;
+  transition: background 0.2s ease-in-out;
+  cursor: pointer;
+  font-weight: 500;
+  :hover {
+    background: #dfdfdf6c;
+  }
+`;
+const Icon = styled.div`
+  display: flex;
+  margin-left: auto;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 25px;
+  margin-top: 15px;
+  cursor: pointer;
+  padding: 10px;
+  border-radius: 50%;
+  transition: background 0.2s ease-in-out;
+  width: 40px;
+  height: 40px;
+  :hover {
+    background: #dfdfdf6c;
+  }
+`;
---
+++ src/components/Section.jsx
@@ -0,0 +1,90 @@
+import styled from "styled-components";
+
+import arrow from "../images/down-arrow.svg";
+import aos from "aos";
+
+const Section = ({ car }) => {
+  return (
+    <Wrapper img={car.image}>
+      <Hero_Text>
+        <Hero_Header>{car.title}</Hero_Header>
+        <Sub_Header>{car.desc}</Sub_Header>
+      </Hero_Text>
+      <Hero_Buttons>
+        <Hero_LeftBtn>{car.buttonLeft}</Hero_LeftBtn>
+        {car.buttonRight && <Hero_RightBtn>{car.buttonRight}</Hero_RightBtn>}
+      </Hero_Buttons>
+      <DownArrow src={arrow} alt="Down Arrow" />
+    </Wrapper>
+  );
+};
+
+export default Section;
+
+const Wrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-image: url(${(props) => props.img});
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  overflow-x: hidden;
+`;
+
+const Hero_Text = styled.div`
+  text-align: center;
+  margin-top: 15vh;
+`;
+const Hero_Header = styled.h1``;
+const Sub_Header = styled.p``;
+
+const Hero_Buttons = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: auto;
+  margin-bottom: 10px;
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
+`;
+const Hero_LeftBtn = styled.button`
+  background-color: #171a20cc;
+  height: 40px;
+  width: 256px;
+  color: #fff;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  border: none;
+  outline: none;
+  border-radius: 100px;
+  opacity: 0.85;
+  text-transform: uppercase;
+  font-size: 12px;
+  cursor: pointer;
+  margin: 0 15px;
+  :hover {
+    opacity: 1;
+  }
+  font-weight: 700;
+  letter-spacing: 2px;
+  @media (max-width: 768px) {
+    margin: 10px 0;
+  }
+`;
+const Hero_RightBtn = styled(Hero_LeftBtn)`
+  background-color: #ffffffcc;
+  color: #000;
+  opacity: 0.65;
+`;
+
+const DownArrow = styled.img`
+  width: 40px;
+  height: 40px;
+  margin-bottom: 20px;
+`;
